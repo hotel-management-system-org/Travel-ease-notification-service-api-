52463999ae532a045cc024f2b6d8df9815fab28a
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 21:07:23 +0530

    feat: add EmailConfig with SMTP settings and sender details using environment variables

---
+++ src/config/email.config.ts
@@ -0,0 +1,16 @@
+export const EmailConfig = {
+    smtp: {
+        host: process.env.SMTP_HOST || 'smtp.gmail.com',
+        port: parseInt(process.env.SMTP_PORT || '587', 10),
+        secure: process.env.SMTP_SECURE === "true",
+        auth: {
+            user: process.env.SMTP_USER || '',
+            pass: process.env.SMTP_PASSWORD || '',
+        },
+    },
+
+    from: {
+        email: process.env.EMAIL_FROM || 'devboot437@gmail.com',
+        name: 'Travel Ease Hotel System',
+    }
+};
